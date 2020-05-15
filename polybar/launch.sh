f07b85f5d60309440b3a380f5f52cb33de6dba60
andrewkreuzer <akreuzer.08@gmail.com>
2020-05-15 10:16:25 -0400

    bspwm, polybar, sxhkd setup and bspwm scratchpad

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,8 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "----" | tee -a /tmp/polybar_top.log /tmp/polybar_bottom.log
-polybar top -c ~/.config/polybar/config-top.ini 2>&1 &
-#polybar bottom -c ~/.config/polybar/config-bottom.ini 2>&1 &
+echo "----" | tee -a /tmp/polybar_main.log /tmp/polybar_right.log
+polybar main -c ~/.config/polybar/config-main-monitor.ini 2>&1 &
+polybar right -c ~/.config/polybar/config-right-monitor.ini 2>&1 &
 
 echo "Bars launched..."
