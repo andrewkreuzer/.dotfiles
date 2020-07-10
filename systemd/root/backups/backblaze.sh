74ab6c6b844b829fc6bfc28c802cb18c9a816d17
andrewkreuzer <akreuzer.08@gmail.com>
2020-07-10 17:02:10 -0400

    sanoid for zfs snapshoting and rclone scripts for backup to backblaze

---
+++ systemd/root/backups/backblaze.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+RCLONE_CONFIG_PASS=$(su - akreuzer -c 'pass rclone/config')
+APP_TOKEN=$(su - akreuzer -c 'pass pushover/croft-backup')
+USER_KEY=$(su - akreuzer -c 'pass pushover/user-key')
+
+dirs=(
+    "/home/akreuzer/dev/" 
+    "/home/akreuzer/Documents/"
+    "/etc"
+)
+
+
+for d in ${dirs[@]}; do
+    SECONDS=0
+    rclone --config="/home/akreuzer/.config/rclone/rclone.conf" sync --password-command="echo $RCLONE_CONFIG_PASS" $d croft-encrypt:$d
+    duration=$SECONDS
+    curl -s \
+             --form-string "token=$APP_TOKEN" \
+             --form-string "user=$USER_KEY" \
+             --form-string "message=$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed for $d" \
+             https://api.pushover.net/1/messages.json
+done
