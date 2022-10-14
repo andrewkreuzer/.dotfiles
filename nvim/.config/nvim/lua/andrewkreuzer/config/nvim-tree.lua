faef25d441570fc081bf98a9ae9cddfe2cff408f
andrewkreuzer <me@andrewkreuzer.com>
2022-10-13 21:03:30 -0400

    nvim tree open on start

--- nvim/.config/nvim/lua/andrewkreuzer/config/nvim-tree.lua
+++ nvim/.config/nvim/lua/andrewkreuzer/config/nvim-tree.lua
@@ -1,4 +1,10 @@
-require('nvim-tree').setup {}
+require('nvim-tree').setup({
+  open_on_setup = true,
+  open_on_setup_file = true,
+  view = {
+    adaptive_size = true,
+  }
+})
 
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-f>', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
