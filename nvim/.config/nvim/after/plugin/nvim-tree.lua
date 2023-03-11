c8bded8e61801fb6ca16094c94428b0269cd6c65
andrewkreuzer <me@andrewkreuzer.com>
2023-03-11 13:54:12 -0500

    Update depricated nvim-tree open command to function

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -1,10 +1,13 @@
 require('nvim-tree').setup({
-  open_on_setup = true,
-  open_on_setup_file = true,
   view = {
     adaptive_size = true,
   }
 })
 
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-f>', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
