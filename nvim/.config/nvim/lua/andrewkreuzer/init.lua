cdd83f2913b45840c5c83dcd29bcec570484aa12
andrewkreuzer <me@andrewkreuzer.com>
2024-02-11 14:27:45 -0500

    Add config value to remove mason related things
    nix doesn't support mason very well

--- nvim/.config/nvim/lua/andrewkreuzer/init.lua
+++ nvim/.config/nvim/lua/andrewkreuzer/init.lua
@@ -1,44 +1,50 @@
-local opt = require('andrewkreuzer.utils').opt
-local cmd = vim.cmd
+local M = {}
 
-vim.g.loaded = 1
-vim.g.loaded_netrwPlugin = 1
+M.start = function(nix)
+  local opt = require('andrewkreuzer.utils').opt
+  local cmd = vim.cmd
 
-cmd([[ syntax enable ]])
-cmd([[ hi Visual cterm=reverse ctermbg=NONE ]])
-cmd([[ hi ColorColumn ctermbg=0 guibg=lightgrey ]])
-opt('colorcolumn', '80')
-opt('mouse', 'a')
-opt('number')
-opt('relativenumber')
-opt('wrap', false)
-opt('hlsearch')
-opt('incsearch')
-opt('ignorecase')
-opt('hidden')
-opt('smartcase')
-opt('cmdheight', 1)
-opt('cursorline')
-opt('splitbelow')
-opt('splitright')
-opt('tabstop', 2)
-opt('softtabstop', 2)
-opt('shiftwidth', 2)
-opt('expandtab')
-opt('smarttab')
-opt('autoindent')
-opt('list')
-cmd([[ filetype plugin on ]])
+  vim.g.loaded = 1
+  vim.g.loaded_netrwPlugin = 1
 
-vim.g.mapleader = [[ ]]
+  cmd([[ syntax enable ]])
+  cmd([[ hi Visual cterm=reverse ctermbg=NONE ]])
+  cmd([[ hi ColorColumn ctermbg=0 guibg=lightgrey ]])
+  opt('colorcolumn', '80')
+  opt('mouse', 'a')
+  opt('number')
+  opt('relativenumber')
+  opt('wrap', false)
+  opt('hlsearch')
+  opt('incsearch')
+  opt('ignorecase')
+  opt('hidden')
+  opt('smartcase')
+  opt('cmdheight', 1)
+  opt('cursorline')
+  opt('splitbelow')
+  opt('splitright')
+  opt('tabstop', 2)
+  opt('softtabstop', 2)
+  opt('shiftwidth', 2)
+  opt('expandtab')
+  opt('smarttab')
+  opt('autoindent')
+  opt('list')
+  cmd([[ filetype plugin on ]])
 
-vim.opt.termguicolors = true
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+  vim.g.mapleader = [[ ]]
 
-require('andrewkreuzer.plugins')
-require('andrewkreuzer.theme')
-require('andrewkreuzer.keymaps')
-require('andrewkreuzer.autocmds')
+  vim.opt.termguicolors = true
+  vim.opt.swapfile = false
+  vim.opt.backup = false
+  vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+  vim.opt.undofile = true
+
+  require('andrewkreuzer.plugins').setup(nix)
+  require('andrewkreuzer.theme')
+  require('andrewkreuzer.keymaps')
+  require('andrewkreuzer.autocmds')
+end
+
+return M
