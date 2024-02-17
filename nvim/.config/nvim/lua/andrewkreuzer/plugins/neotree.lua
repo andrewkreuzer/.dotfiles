c62f6060579d49fdaf0efac4e913fa343968dbe7
andrewkreuzer <me@andrewkreuzer.com>
2024-02-17 11:16:52 -0500

    neotree options

--- nvim/.config/nvim/lua/andrewkreuzer/plugins/neotree.lua
+++ nvim/.config/nvim/lua/andrewkreuzer/plugins/neotree.lua
@@ -2,7 +2,14 @@ local M = {}
 
 M.setup = function()
   require('neo-tree').setup({
+    open_files_do_not_replace_types = { "terminal", "trouble", "qf", "fugitive" },
     filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+        },
+      },
       follow_current_file = {
         enabled = true,
       },
